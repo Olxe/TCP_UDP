32946e2c0fb5fbc61f1757c783bee4df36fee30f
Bugnone <bugnone.michael@gmail.com>
2020-11-23 08:49:34 +0100

    first

---
+++ src/ClientUDP.java
@@ -0,0 +1,5 @@
+public class ClientUDP {
+    public static void main(String[] args) {
+        System.out.println("Client UDP");
+    }
+}
---
+++ src/ServerUDP.java
@@ -0,0 +1,5 @@
+public class ServerUDP {
+    public static void main(String[] args) {
+        System.out.println("Server UDP");
+    }
+}
