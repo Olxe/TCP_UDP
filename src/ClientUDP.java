165b1b5238c6b07fef94b3e6831e059c6574b4bf
Bugnone <bugnone.michael@gmail.com>
2020-11-23 09:45:25 +0100

    udp, client, server

--- src/ClientUDP.java
+++ src/ClientUDP.java
@@ -1,5 +1,31 @@
+import java.io.IOException;
+import java.net.*;
+
 public class ClientUDP {
+    public final static int port = 2345;
+
     public static void main(String[] args) {
         System.out.println("Client UDP");
+
+        String str = "NO";
+        byte[] buffer = str.getBytes();
+
+        try {
+            DatagramSocket client = new DatagramSocket();
+
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+
+            packet.setData(buffer);
+
+            client.send(packet);
+
+            byte[] buffer2 = new byte[8196];
+            DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, address, port);
+            client.receive(packet2);
+            System.out.println("Received : " + new String(packet2.getData()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ServerUDP.java
+++ src/ServerUDP.java
@@ -1,5 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ServerUDP {
+    public final static int port = 2345;
+
     public static void main(String[] args) {
         System.out.println("Server UDP");
+
+        try {
+            DatagramSocket server = new DatagramSocket(port);
+
+            while(true) {
+                byte[] buffer = new byte[8192];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                server.receive(packet);
+
+                String str = new String(packet.getData());
+                System.out.println("Received from " + packet.getAddress() + " on " + packet.getPort() + " : " + str);
+
+                packet.setLength(buffer.length);
+
+                byte[] buffer2 = new SimpleDateFormat("HH:mm:ss").format(new Date()).getBytes();
+                DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, packet.getAddress(), packet.getPort());
+
+                server.send(packet2);
+                packet2.setLength(buffer2.length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
